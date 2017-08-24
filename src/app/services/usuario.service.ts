74bd3088895128a332924393a72d2aabbfb709ea
vicosito <vmorales201103@gmail.com>
2017-08-23 22:16:44 -0400

    Limpie codigo muerto del servicio de usuarios

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,29 +1,18 @@
-/**
- * Created by Victor on 12/7/2017.
- */
 import {Injectable} from '@angular/core';
-import {Http, Response, Headers} from '@angular/http';
+import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
-import {Observable} from 'rxjs/observable';
 
 @Injectable()
 export class UsuarioService {
-  public url:string ;
-  public headers = new Headers({'Content-Type': 'application/json'});
-  //private url: string = "http://3cc46610.ngrok.io/users/authenticate";
+  public urlDeAutentifiacionDeUsuarios: string;
+  public jsonHeaders = new Headers({'Content-Type': 'application/json'});
 
   constructor(private  http: Http) {
-    this.url="http://localhost:3000/users/authenticate";
+    this.urlDeAutentifiacionDeUsuarios = "http://localhost:3000/users/authenticate";
   }
-getPrueba(){
-    return 'Hola mundo desde el servicio ';
-}
-getArticulos(){
-  return this.http.get(this.url)
-    .map(respuesta => respuesta.json());
-}
-postUsuario(usuario:string, pass:string ){
-var json = JSON.stringify({"email": usuario, "password": pass});
-return this.http.post(this.url,json,{headers: this.headers}).map(res => res);
-}
+
+  postUsuario(usuario: string, pass: string) {
+    var usuarioJson = JSON.stringify({"email": usuario, "password": pass});
+    return this.http.post(this.urlDeAutentifiacionDeUsuarios, usuarioJson, {headers: this.jsonHeaders}).map(res => res);
+  }
 }
